08c1d01d71cd553a3abd0c1c423d8ff053f5827b
Asher <asherjohn48@gmail.com>
2025-06-24 01:18:09 +0800

    added indexContoller

---
+++ controllers/indexController.js
@@ -0,0 +1,33 @@
+const Products = require('../models/productsModel');
+const Models = require('../models/modelsModel');
+const Categories = require('../models/categoriesModel');
+
+
+const renderDashboard = async (req, res) => {
+  const products = (await Products.retrieveAll());
+  const models = (await Models.retrieveAll());
+  const categories = (await Categories.retrieveAll());
+  const totalLowStock = (await Products.retrieveLowStock());
+
+  res.render('index', {
+    windowTitle: 'Home | RK Inventory',
+    documentTitle: 'Royal Kludge Inventory',
+    header: {
+      icons: {
+        hamburger: '<svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#f5f5f5"> <path d="M120-680v-80h720v80H120Zm0 480v-80h720v80H120Zm0-240v-80h720v80H120Z"/> </svg>',
+        close: '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#f5f5f5"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>',
+      }
+    },
+    content: {
+      name: 'dashboard',
+      data: {
+        totalProducts: products.length,
+        totalModels: models.length,
+        totalCategories: categories.length,
+        totalLowStock: totalLowStock.length
+      },
+    }
+  });
+}
+
+module.exports = renderDashboard;
