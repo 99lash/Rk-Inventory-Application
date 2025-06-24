afdb0e106854e78484550a89774bacad38e19841
Asher <asherjohn48@gmail.com>
2025-06-25 02:12:39 +0800

    removed users & roles router

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const indexRouter = require('./routes/indexRouter');
 const productsRouter = require('./routes/productsRouter');
 const categoriesRouter = require('./routes/categoriesRouter');
-const usersRouter = require('./routes/usersRouter');
 const modelsRouter = require('./routes/modelsRouter');
-const rolesRouter = require('./routes/rolesRouter');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -22,8 +20,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 /* Routers */
-app.use('/roles', rolesRouter);
-app.use('/users', usersRouter);
 app.use('/models', modelsRouter);
 app.use('/categories', categoriesRouter);
 app.use('/products', productsRouter);
