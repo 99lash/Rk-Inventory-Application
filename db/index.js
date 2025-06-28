346f326b0314eed1a1e2478c9c60a005e44f4ba1
Asher <asherjohn48@gmail.com>
2025-06-28 22:57:45 +0800

    production mode

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
-  const { Pool } = require('pg');
+  require('dotenv').config();
 }
-require('dotenv').config();
+const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
